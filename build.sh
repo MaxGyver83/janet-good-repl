e04b77b99b4eb90f70722aacbc2399c816e7ee4e
Max Schillinger <maxschillinger@web.de>
2024-06-19 12:56:49 +0200

    Fix parameter order in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # add -L/usr/local/lib if necessary:
-cc -o janet-good-repl -ljanet shell.c
+cc -o janet-good-repl shell.c -ljanet
 mkdir -p ~/.local/share/janet-good-repl
 touch ~/.local/share/janet-good-repl/history
